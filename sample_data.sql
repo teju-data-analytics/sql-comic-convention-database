3236f2b059d6e1d18bac8eed9f2e7e39e16a1abb
teju-data-analytics <tejupolishetty2@gmail.com>
2026-03-15 15:41:09 -0500

    Add sample data SQL file
    Insert statements for comic convention database tables.

---
+++ sample_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ConventionAttendee VALUES
+(1003, 'Sam', 'Wilson', '1113335555', 'swilson@comicsfan.com', 'A', 'All'),
+(1004, 'Diana', 'Prince', '1114446666', 'dprince@heroes.com', 'A', 'Sat'),
+(1005, 'Bruce', 'Wayne', '2225557777', 'bwayne@gotham.com', 'S', 'All');
